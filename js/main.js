9a0bdf3057420d8427979ebfb1367c970071bfc6
Chris Adkins <c.adkins19967@gmail.com>
2017-03-24 23:14:04 -0400

    basic formatting

--- js/main.js
+++ js/main.js
@@ -1,1 +1,42 @@
-var seven = 7;
+$(".linkOne").click(function() {
+    $(".nav-link").removeClass("active");
+    $(".linkOne").addClass("active");
+});
+$(".linkTwo").click(function() {
+    $(".nav-link").removeClass("active");
+    $(".linkTwo").addClass("active");
+    $("#actualContent").html(`
+                <div class="repoNav col-md-12">
+                    <form>
+                        <input type="text" placeholder="Find a repository">
+                        <button>Search</button>
+                    </form> 
+                    <ul>
+                        <li>All</li>
+                        <li>Public</li>
+                        <li>Private</li>
+                        <li>Sources</li>
+                        <li>Forks</li>
+                        <li>Mirrors</li>
+                    </ul>
+                    <button>New</button></div><hr>
+            `);
+    $.ajax({ url: "https://api.github.com/users/CNAtion96/repos",
+        success: function(response){
+            console.log(response);
+            response.forEach(function(i){
+                var updated = moment(i.updated_at).from();
+                $("#actualContent").append(`<div class="row"><div class="col-md-10">
+                        <h5>${i.name}</h5>
+                        <p id="right">
+                        <p>Updated ${updated}</p></div><div class="col-md-2">
+                        <p></p>
+                        </div></div><hr>`)
+            })
+        }
+    });
+});
+$(".linkThree").click(function() {
+    $(".nav-link").removeClass("active");
+    $(".linkThree").addClass("active");
+});
--- js/main.ts
+++ js/main.ts
@@ -1,1 +1,19 @@
-var seven: number = 7;
+$(".linkOne").click(function(){
+    $(".nav-link").removeClass("active");
+    $(".linkOne").addClass("active");
+})
+
+$(".linkTwo").click(function(){
+    $(".nav-link").removeClass("active");
+    $(".linkTwo").addClass("active");
+    $.ajax({ url: "https://api.github.com/users/CNAtion96/repos",
+        success: fuction(response){
+            console.log(response);
+        }
+    })
+})
+
+$(".linkThree").click(function(){
+    $(".nav-link").removeClass("active");
+    $(".linkThree").addClass("active");
+})
